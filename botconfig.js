ae938f993dba855eca96fcab5ff43d7476487eb6
Dark Overlord <111354371+darklyonteam@users.noreply.github.com>
2024-01-04 20:19:36 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,14 +4,14 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "/",
   Port: 1922, //Which port website gonna be hosted
   SupportServer: "https://discord.io/sciencegear", // Support Server Link
-  Token: process.env.Token || "MTE5MjQwNDc2NzkzNTMxMTk1Mw.Gr8WCn.hbpT3DSJuL4ZUXmaS4sTHFbXDjPhNN2CAYm7z4", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "1192404767935311953", 
-  ClientSecret: process.env.Discord_ClientSecret || "JUV8tx2wzocl6sqRdmu_kwBxPKOYnjuP", 
+  Token: process.env.Token || "MTE5MjQ1NzM1Mjc4MDIwMjA0NA.GYoZtL.d_tK1DMSxocEciJbUa-bwXNNnOa1PyllDrg6Ls", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1192457352780202044", 
+  ClientSecret: process.env.Discord_ClientSecret || "GTJhXYjS9YePCgFL3ac0tOW9cLbOcNhf", 
   Scopes: ["identify", "guilds", "applications.commands"],
   ServerDeafen: true, 
   DefaultVolume: 100,
   CallbackURL: "/api/callback", 
-  "24/7": false,
+  "24/7": true,
   CookieSecret: "Pikachu is cute",
   IconURL:
     "https://media.tenor.com/sce9SDRey4EAAAAi/disc.gif",
